f868dca8fd374904f1613f7410e6250230163582
Younes el Anjri <47318738+Younesanjri@users.noreply.github.com>
2020-02-26 11:27:16 +0100

    Create MyLogger.class.php
    initial commit

---
+++ MyLogger.class.php
@@ -0,0 +1,27 @@
+<?php
+class MyLogger {
+    public function warning($message) {
+        $this->log("WARNING: " . $message);
+    }
+
+    public function error($message) {
+        $this->log("ERROR: " . $message);
+    }
+
+    public function info($message) {
+        $this->log("INFO: " . $message);
+    }
+
+    public function debug($message) {
+        $this->log("DEBUG: " . $message);
+    }
+
+    private function log($message) {
+        print($message);
+    }
+}
+
+$logger = new MyLogger();
+
+$logger->error("test")
+?>
